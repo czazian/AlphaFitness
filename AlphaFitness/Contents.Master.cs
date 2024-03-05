2743267c7151136bf2c9fb7b3f9b2fb0ab584142
ささやかな風と青い海 <99159488+czazian@users.noreply.github.com>
2024-03-05 09:24:11 +0800

    Minor fixed for Weather API

--- AlphaFitness/Contents.Master.cs
+++ AlphaFitness/Contents.Master.cs
@@ -119,29 +119,29 @@ namespace AlphaFitness
 
     public class WeatherInfo
     {
-        public float app_temp { get; set; }
-        public int aqi { get; set; }
+        public double app_temp { get; set; }
+        public double aqi { get; set; }
         public string city_name { get; set; }
-        public int clouds { get; set; }
+        public double clouds { get; set; }
         public string country_code { get; set; }
         public string datetime { get; set; }
-        public float dewpt { get; set; }
-        public int dhi { get; set; }
-        public int dni { get; set; }
+        public double dewpt { get; set; }
+        public double dhi { get; set; }
+        public double dni { get; set; }
         public double elev_angle { get; set; }
-        public int ghi { get; set; }
-        //public double gust { get; set; }
+        public double ghi { get; set; }
+        public double gust { get; set; }
         public double h_angle { get; set; }
         public double lat { get; set; }
         public double lon { get; set; }
         public string ob_time { get; set; }
         public string pod { get; set; }
-        public long precip { get; set; }
-        public float pres { get; set; }
-        public int rh { get; set; }
-        public float slp { get; set; }
-        public int snow { get; set; }
-        public int solar_rad { get; set; }
+        public double precip { get; set; }
+        public double pres { get; set; }
+        public double rh { get; set; }
+        public double slp { get; set; }
+        public double snow { get; set; }
+        public double solar_rad { get; set; }
         public List<string> sources { get; set; }
         public string state_code { get; set; }
         public string station { get; set; }
@@ -149,9 +149,9 @@ namespace AlphaFitness
         public string sunset { get; set; }
         public float temp { get; set; }
         public string timezone { get; set; }
-        public long ts { get; set; }
-        public int uv { get; set; }
-        public int vis { get; set; }
+        public double ts { get; set; }
+        public double uv { get; set; }
+        public double vis { get; set; }
         public WeatherDetails weather { get; set; }
         public WindInfo wind { get; set; }
     }
