4fd674b69a54539c96e90a344950003a1fc133e2
ささやかな風と青い海 <99159488+czazian@users.noreply.github.com>
2024-03-12 21:18:49 +0800

    db

--- AlphaFitness/Community/Community.aspx.cs
+++ AlphaFitness/Community/Community.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +13,26 @@ namespace AlphaFitness.Community
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            SqlConnection conn;
+            string str = ConfigurationManager.ConnectionStrings["AlphaFitness"].ConnectionString;
+            conn = new SqlConnection(str);
 
+            conn.Open();
+
+            string retrieve = "SELECT * " +
+                "FROM Post p, User u, Comment c " +
+                "WHERE u.UserID =  p.UserID" +
+                "AND u.UserID = c.UserID";
+
+            SqlCommand cmd = new SqlCommand(retrieve, conn);
+
+            SqlDataReader item = cmd.ExecuteReader(); //More than 1 value
+
+            if (item.HasRows)
+            {
+                itemRepeater.DataSource = item;
+                itemRepeater.DataBind();
+            }
         }
     }
 }
